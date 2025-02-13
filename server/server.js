eab92de9a5d1473141bb80063b155d000a6beff3
Jenifer <85919892+jenifer98k@users.noreply.github.com>
2025-02-13 18:42:55 +0530

    Update v2  server.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
-
+const cors = require('cors');
 // Initialize the Express app
 const app = express();
 const port = process.env.PORT || 4000;
@@ -23,14 +23,14 @@ const allowedOrigins = [
 // Middleware
 app.use(express.json());// all req wil be passed in json format
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true, // Allow cookies
-    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
-    allowedHeaders: ["Content-Type", "Authorization"], // Allow headers
-  })
-);
+// Allow requests only from your frontend domain
+const corsOptions = {
+    origin: 'https://git-12-client.vercel.app',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization'
+};
+
+app.use(cors(corsOptions));
 
 // API Endpoints 
 app.get("/", (req, res) => {
