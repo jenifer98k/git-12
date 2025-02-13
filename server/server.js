110e363619b2205c4c615ec276b4cc5f24a93fdf
jenifer <joyjenifermiracle@gmail.comgit config --global user.email>
2025-02-13 17:18:45 +0530

    updated url in .env

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ import userRouter from "./routes/userRoutes.js";
 const app = express();
 const port = process.env.PORT || 4000;
 connectDB() 
-const allowedOrigins =  ['http://localhost:5173']
+const allowedOrigins = [process.env.FRONTEND_URL];
 // Middleware
 app.use(express.json());// all req wil be passed in json format
 app.use(cookieParser());
